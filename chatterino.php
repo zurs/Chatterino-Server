6c51f78c844027e91de64990922e2459eabb4446
Zurs <hampusek96@gmail.com>
2015-04-18 03:10:19 +0000

    First commit, basic function & no testing

---
+++ chatterino.php
@@ -0,0 +1,45 @@
+<?php
+require("scripts.php");
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+
+$config = simplexml_load_file("config.xml");
+$dbhost = $config->dbhost;
+$dbname = $config->dbname;
+$dbusername = $config->dbusername;
+$dbpasswd = $config->dbpasswd;
+
+$dbh = new PDO("mysql:host={$dbhost}; charset=utf8; dbname={$dbname};", $dbusername, $dbpasswd);
+
+$post_data = @file_get_contents("php://input");
+$data = json_decode($post_data);
+$nickname = $data->nickname;
+$password = $data->password;
+$function = $data->function;
+
+if($function == "createUser"){ // Om en ny användare skulle vilja skapas
+    createUser($dbh, $nickname, $password);
+}
+else{
+    if(authorize($dbh, $username, $password)){ // Autentierar användaren och tillåter ytterliga funktioner
+        
+        switch($function){
+            case "connection":
+                newConnection($dbh, $username);
+                break;
+            case "checkForNewMessages":
+                $lastMessage = $data->lastMessage;
+                checkForNewMessages($dbh, $lastMessage);
+                break;
+            case "newMessage":
+                $messageText = $data->messageText;
+                $senderNick = $data->username;
+                newMessage($dbh, $senderNick, $messageText);
+                break;
+        }
+        
+    }
+}
+
+
+?>
---
+++ scripts.php
@@ -0,0 +1,67 @@
+<?php
+
+function createUser($dbh, $nickname, $password){
+    $sql = "INSERT INTO users (nickname, password) VALUES ('" + $nickname + "', '" + $password + "');";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    echo("true");
+}
+
+function authorize($dbh, $nickname, $password){
+    $sql = "SELECT password FROM users WHERE username='" + $username + "';";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    if($password == $stmt->fetch["password"]){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkForNewMessages($dbh, $lastMessage){
+    $sql = "SELECT messageID FROM chat ORDER BY datetime LIMIT 1;";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    if($lastMessage == $stmt->fetch["messageID"]){
+        echo("false");
+    }
+    else{
+        returnLastMessages($dbh, $lastMessage);
+    }
+}
+
+function returnLastMessages($dbh, $lastMessage){
+    $sql = "SELECT * FROM chat ORDER BY datetime LIMIT 20;";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    
+    $iterations = 0;
+    while($message = $stmt->fetch()){
+        if($message["messageID"] == $lastMessage){
+            returnAmountMessages($dbh, $iterations);
+        }
+        $iterations = $iterations + 1;
+    }
+}
+
+function returnAmountMessages($dbh, $amount){
+    $sql = "SELECT * FROM chat ORDER BY datetime LIMIT " + $amount;
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    echo(json_encode($stmt->fetchAll(PDO::FETCH_ASSOC)));
+}
+
+function newConnection($dbh, $username){
+    $sql = "INSERT INTO chat (senderNick, messageID, messageText, datetime) VALUES ('Server', NULL, '" + $username + " just connected', NOW());";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+}
+
+function newMessage($dbh, $senderNick, $messageText){
+    $sql = "INSERT INTO chat (senderNick, messageID, messageText, datetime) VALUES ('" + $senderNick + "', NULL, '" + $messageText + "', NOW());";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+}
+
+?>
